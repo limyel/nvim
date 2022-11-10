22de03e3d0c6ffb80c45aab48719e3639b56916a
limyel <limyel@outlook.com>
2022-11-10 16:13:56 +0800

    keybindings

---
+++ lua/keybindings.lua
@@ -0,0 +1,76 @@
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+--
+-- vim.api.nvim_set_keymap(): 全局快捷键
+-- vim.api.nvim_buf_set_keymap(): Buffer 快捷键
+
+-- leader key 为空
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- 复用 opt 参数
+local opt = {
+  noremap = true,
+  silent = true,
+}
+
+-- 本地变量
+local map = vim.api.nvim_set_keymap
+
+-- $跳到行尾不带空格 (交换$ 和 g_)
+map("v", "$", "g_", opt)
+map("v", "g_", "$", opt)
+map("n", "$", "g_", opt)
+map("n", "g_", "$", opt)
+
+-- 命令行下 Ctrl+j/k  上一个下一个
+map("c", "<C-j>", "<C-n>", { noremap = false })
+map("c", "<C-k>", "<C-p>", { noremap = false })
+
+-- 保存、退出
+map("n", "<leader>w", ":w<CR>", opt)
+map("n", "<leader>wq", ":wqa!<CR>", opt)
+
+-- fix :set wrap
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+
+-- 上下滚动浏览
+map("n", "<C-j>", "5j", opt)
+map("n", "<C-k>", "5k", opt)
+map("v", "<C-j>", "5j", opt)
+map("v", "<C-k>", "5k", opt)
+-- ctrl u / ctrl + d  只移动9行，默认移动半屏
+map("n", "<C-u>", "10k", opt)
+map("n", "<C-d>", "10j", opt)
+
+-- magic search
+map("n", "/", "/\\v", { noremap = true, silent = false })
+map("v", "/", "/\\v", { noremap = true, silent = false })
+
+-- visual模式下缩进代码
+map("v", "<", "<gv", opt)
+map("v", ">", ">gv", opt)
+
+-- 上下移动选中文本
+map("v", "J", ":move '>+1<CR>gv-gv", opt)
+map("v", "K", ":move '<-2<CR>gv-gv", opt)
+
+-- 在visual mode 里粘贴不要复制
+map("v", "p", '"_dP', opt)
+
+-- 退出
+map("n", "qq", ":q!<CR>", opt)
+map("n", "<leader>q", ":qa!<CR>", opt)
+
+-- insert 模式下，跳到行首行尾
+-- map("i", "<C-h>", "<ESC>I", opt)
+-- map("i", "<C-l>", "<ESC>A", opt)
+
+
+
