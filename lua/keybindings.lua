e801a008888fa38bf871e7667e5212e1d23af961
limyel <limyel@outlook.com>
2022-11-10 16:20:54 +0800

    窗口快捷键“

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -72,5 +72,50 @@ map("n", "<leader>q", ":qa!<CR>", opt)
 -- map("i", "<C-h>", "<ESC>I", opt)
 -- map("i", "<C-l>", "<ESC>A", opt)
 
+------------------------------------------------------------------
+-- windows 分屏快捷键
+------------------------------------------------------------------
+-- 取消 s 默认功能
+map("n", "s", "", opt)
+map("n", "sv", ":vsp<CR>", opt)
+map("n", "sh", ":sp<CR>", opt)
+-- 关闭当前
+map("n", "sc", "<C-w>c", opt)
+-- 关闭其他
+map("n", "so", "<C-w>o", opt)
+-- alt + hjkl  窗口之间跳转
+map("n", "<A-h>", "<C-w>h", opt)
+map("n", "<A-j>", "<C-w>j", opt)
+map("n", "<A-k>", "<C-w>k", opt)
+map("n", "<A-l>", "<C-w>l", opt)
+-- <leader> + hjkl 窗口之间跳转
+map("n", "<leader>h", "<C-w>h", opt)
+map("n", "<leader>j", "<C-w>j", opt)
+map("n", "<leader>k", "<C-w>k", opt)
+map("n", "<leader>l", "<C-w>l", opt)
+-- 左右比例控制
+map("n", "<C-Left>", ":vertical resize -2<CR>", opt)
+map("n", "<C-Right>", ":vertical resize +2<CR>", opt)
+map("n", "s,", ":vertical resize -10<CR>", opt)
+map("n", "s.", ":vertical resize +10<CR>", opt)
+-- 上下比例
+map("n", "sj", ":resize +10<CR>", opt)
+map("n", "sk", ":resize -10<CR>", opt)
+map("n", "<C-Down>", ":resize +2<CR>", opt)
+map("n", "<C-Up>", ":resize -2<CR>", opt)
+-- 相等比例
+map("n", "s=", "<C-w>=", opt)
 
-
+-- Terminal相关
+map("n", "st", ":sp | terminal<CR>", opt)
+map("n", "stv", ":vsp | terminal<CR>", opt)
+-- Esc 回 Normal 模式
+map("t", "<Esc>", "<C-\\><C-n>", opt)
+map("t", "<A-h>", [[ <C-\><C-N><C-w>h ]], opt)
+map("t", "<A-j>", [[ <C-\><C-N><C-w>j ]], opt)
+map("t", "<A-k>", [[ <C-\><C-N><C-w>k ]], opt)
+map("t", "<A-l>", [[ <C-\><C-N><C-w>l ]], opt)
+map("t", "<leader>h", [[ <C-\><C-N><C-w>h ]], opt)
+map("t", "<leader>j", [[ <C-\><C-N><C-w>j ]], opt)
+map("t", "<leader>k", [[ <C-\><C-N><C-w>k ]], opt)
+map("t", "<leader>l", [[ <C-\><C-N><C-w>l ]], opt)
