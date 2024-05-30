76c14c31655c281c06fdd9dd639f78feac911d11
limyel <limyel@outlook.com>
2024-05-30 15:07:04 +0800

    目录结构调整

--- init.lua
+++ init.lua
@@ -1,21 +1,4 @@
 require('basic')
+require('plugins')
+require('theme')
 
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup(
-    { "projekt0n/github-nvim-theme", name = "github-theme", priority = 1000 }
-)
-
-vim.cmd.colorscheme("github_light")
